35164a363de6c41faa3e93d9ee68531ef18b3687
31-ryougishiki <3154764788@qq.com>
2026-04-14 11:11:37 +0800

    fix: rewrite def.cpp with correct OpDef pattern

--- csrc/qwen3_next_qkv_preprocess/op_host/qwen3_next_qkv_preprocess_def.cpp
+++ csrc/qwen3_next_qkv_preprocess/op_host/qwen3_next_qkv_preprocess_def.cpp
@@ -1,4 +1,5 @@
 /**
+ * This program is free software; you can redistribute it and/or modify it.
  * Copyright (c) 2025 Huawei Technologies Co., Ltd.
  * This file is a part of the CANN Open Software.
  * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
@@ -8,59 +9,102 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 
-#include "acl/acl.h"
-#include "aclnn_ops.h"
-
-// ACLNN API definition for qwen3_next_qkv_preprocess
-// This is the host-side API that will be called from PyTorch
-
-extern "C" {
-
-/**
- * @brief Qwen3Next Fused Attention Operator
- *
- * This operator implements the fused attention computation for Qwen3Next models:
- * 1. QKV split (with optional gate splitting)
- * 2. Q RMSNorm
- * 3. K RMSNorm
- * 4. Rotary Embedding
- * 5. Attention computation
- * 6. Optional gate multiplication
- *
- * @param qkv[input] - QKV tensor after projection (numTokens, qkvSize)
- * @param qNormWeight[input] - Q normalization weight (headDim)
- * @param kNormWeight[input] - K normalization weight (headDim)
- * @param qCos[input] - Q rotary embedding cos (numTokens, headDim)
- * @param qSin[input] - Q rotary embedding sin (numTokens, headDim)
- * @param kCos[input] - K rotary embedding cos (numTokens, headDim)
- * @param kSin[input] - K rotary embedding sin (numTokens, headDim)
- * @param gate[input] - Optional gate tensor (numTokens, qSize), null if attnOutputGate is false
- * @param output[output] - Attention output (numTokens, hiddenSize)
- * @param workspace[workspace] - Workspace buffer
- * @param tiling[tiling] - Tiling data
- *
- * @return aclnnStatus - ACLNN status code
+/*!
+ * \file qwen3_next_qkv_preprocess_def.cpp
+ * \brief Op definition for Qwen3NextQKVPreprocess
  */
-aclnnStatus aclnnQwen3NextQKVPreprocess(
-    const aclTensor* qkv,
-    const aclTensor* qNormWeight,
-    const aclTensor* kNormWeight,
-    const aclTensor* qCos,
-    const aclTensor* qSin,
-    const aclTensor* kCos,
-    const aclTensor* kSin,
-    const aclTensor* gate,  // optional, can be null
-    const aclScalar* epsilon,
-    const aclIntArray* numTokens,
-    const aclIntArray* numHeads,
-    const aclIntArray* numKvHeads,
-    const aclIntArray* headDim,
-    const aclIntArray* qSize,
-    const aclIntArray* kvSize,
-    const aclIntArray* hiddenSize,
-    const aclIntArray* qkvSize,
-    const aclIntArray* attnOutputGate,
-    aclTensor* output,
-    aclBaseSpace* workspace,
-    const aclTensor* tiling);
-}  // extern "C"
+
+#include "register/op_def_registry.h"
+
+namespace ops {
+class Qwen3NextQKVPreprocess : public OpDef {
+public:
+    explicit Qwen3NextQKVPreprocess(const char* name) : OpDef(name)
+    {
+        this->Input("qkv")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Input("qNormWeight")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Input("kNormWeight")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Input("qCos")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Input("qSin")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Input("kCos")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Input("kSin")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Input("gate")
+            .ParamType(OPTIONAL)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+
+        this->Output("qOut")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Output("kOut")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Output("vOut")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+        this->Output("gateOut")
+            .ParamType(OPTIONAL)
+            .DataType({ge::DT_FLOAT16, ge::DT_BF16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND})
+            .AutoContiguous();
+
+        this->Attr("epsilon").AttrType(OPTIONAL).Float(1e-6);
+        this->Attr("numTokens").AttrType(OPTIONAL).Int(0);
+        this->Attr("numHeads").AttrType(OPTIONAL).Int(0);
+        this->Attr("numKvHeads").AttrType(OPTIONAL).Int(0);
+        this->Attr("headDim").AttrType(OPTIONAL).Int(0);
+        this->Attr("qSize").AttrType(OPTIONAL).Int(0);
+        this->Attr("kvSize").AttrType(OPTIONAL).Int(0);
+        this->Attr("qkvSize").AttrType(OPTIONAL).Int(0);
+        this->Attr("attnOutputGate").AttrType(OPTIONAL).Int(0);
+
+        OpAICoreConfig aicoreConfig;
+        aicoreConfig.DynamicCompileStaticFlag(true)
+            .DynamicFormatFlag(false)
+            .DynamicRankSupportFlag(true)
+            .DynamicShapeSupportFlag(true)
+            .NeedCheckSupportFlag(false)
+            .PrecisionReduceFlag(true)
+            .ExtendCfgInfo("coreType.value", "AiCore");
+        this->AICore().AddConfig("ascend910b", aicoreConfig);
+        this->AICore().AddConfig("ascend910_93", aicoreConfig);
+    }
+};
+OP_ADD(Qwen3NextQKVPreprocess);
+
+} // namespace ops
